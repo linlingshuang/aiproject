0c9db7e12111a6626bff1f933acf7feac45f9a34
LinLingshuang <2025302114225@whu.edu.cn>
2026-04-10 23:30:22 +0800

    Update FlappyBird.cpp

--- AiPlayFlappyBird/FlappyBird/FlappyBird.cpp
+++ AiPlayFlappyBird/FlappyBird/FlappyBird.cpp
@@ -19,15 +19,34 @@ int main()
 	Sprite/*指可移动图像*/b2(background);
 	b1.setTextureRect(IntRect(0, 0, 256, 384));
 	b2.setTextureRect(IntRect(0, 0, 256, 384));
-	int backgroudmove = 0;
+	int backgroudMove = 0;
 
-	float tickTime = 0, delay = 0.1;
+	Texture pipe;
+	pipe.loadFromFile("pipe.png");
+
+	Sprite/*指可移动图像*/upPipe(pipe);
+	Sprite/*指可移动图像*/downPipe(pipe);
+	upPipe.setTextureRect(IntRect(0, 0, 60, 384));
+	downPipe.setTextureRect(IntRect(0, 0, 60, 384));
+	int pipeMove = 0;
+
+	upPipe.setRotation(180);
+
+	float tickTime = 0, delay = 0.05;
 
 	int state = 0;
 	float birdRotate = 0;
 	bool flyup = true, rotateup = true;
 
 	Clock clock;
+
+	int upPipeArea= 20 + rand() % 200;
+	int downPipeArea= upPipeArea+100;
+	//int upPipeArea = 50;
+	//int downPipeArea = upPipeArea + 100;
+
+	int birdHeight = 128 - 24;
+	int birdx = 68 + 34;
 
 	while (window.isOpen())
 	{
@@ -36,15 +55,18 @@ int main()
 		clock.restart();
 		tickTime += time;
 
+
 		Event gameEvent;
 		while (window.pollEvent(gameEvent))
 		{
 			if (gameEvent.type == Event::Closed) {
 				window.close();
 			}
-
+			//按键
+			if (gameEvent.type == Event::KeyPressed) {
+				if (gameEvent.key.code == Keyboard::W)birdHeight-=7;
+			}
 		}
-
 		//下落间隔
 		if (tickTime > delay) {
 			tickTime = 0;
@@ -61,6 +83,7 @@ int main()
 					state = 1;
 					flyup = true;
 				}
+				birdHeight++;
 			}
 			/*
 			if (rotateup) {
@@ -78,24 +101,42 @@ int main()
 				}
 			}
 			*/
-			backgroudmove+=2;
-			if (backgroudmove == 256)
-				backgroudmove = 0;
+			backgroudMove++ ;
+			pipeMove+= 2;
+			if (backgroudMove == 256)backgroudMove = 0;
+			if (pipeMove == 256 + 60) {
+				upPipeArea = 20 + rand() % 200;
+				downPipeArea = upPipeArea + 100;
+				pipeMove = 0;
+			}
 		}
 
 		//绘制
 		window.clear(Color::White);
-		b1.setPosition(0 - backgroudmove, 0);
+		b1.setPosition(0 - backgroudMove, 0);
 		window.draw(b1);
-		b2.setPosition(256 - backgroudmove, 0);
+		b2.setPosition(256 - backgroudMove, 0);
 		window.draw(b2);
 
+		downPipe.setPosition(256 - pipeMove, downPipeArea);
+		window.draw(downPipe);
+		upPipe.setPosition(256 - pipeMove + 60, upPipeArea);
+		window.draw(upPipe);
+
+		if ((birdHeight + 24 >= downPipeArea || birdHeight <= upPipeArea) && (birdx >= 256 - pipeMove && birdx <= 256 - pipeMove + 60)) {
+			state = 3;
+		}
+
+
 		s.setTextureRect(IntRect(34 * state, 0, 34, 24)); // 矩形范围
-		s.setPosition(128 - 34, 128 - 24);
+		s.setPosition(birdx-34, birdHeight);
 		window.draw(s);
 
 		window.display();
-
+		if (state == 3) {
+			state = 0;
+			flyup = true;
+		}
 	}
 
 	return 0;
